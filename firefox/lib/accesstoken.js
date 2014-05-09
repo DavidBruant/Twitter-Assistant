fe1fe3a06474e2abfa39a95ba1a73e3ca02929c1
David Bruant <bruant.d@gmail.com>
2014-05-09 18:40:34 +0200

    https://api.twitter.com/oauth2/token always 403s if the request contains the Cookie header which is send by default by HTTP reqs made through the Request module. This commit uses an anynomized XHR.

--- firefox/lib/accesstoken.js
+++ firefox/lib/accesstoken.js
@@ -1,7 +1,7 @@
 'use strict';
 // https://dev.twitter.com/docs/auth/application-only-auth
 // RFC 1738
-var Request = require("sdk/request").Request;
+var XMLHttpRequest = require("sdk/net/xhr").XMLHttpRequest;
 var base64 = require("sdk/base64");
 const { defer } = require('sdk/core/promise');
 
@@ -22,26 +22,37 @@ module.exports = function getAccessToken(CONSUMER_KEY, CONSUMER_SECRET){
         accessTokenDef.resolve(tokenCache.get(bearerTokenCredentails));
     }
     else{
+        // Using ("sdk/net/xhr").XMLHttpRequest until https://bugzilla.mozilla.org/show_bug.cgi?id=1002229 ships to release (Firefox 32?)
+        var xhr = new XMLHttpRequest({mozAnon: true});
         
-        Request({
-            url: "https://api.twitter.com/oauth2/token",
-            headers: {
-                'Authorization': 'Basic '+b64bearerToken,
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-                'Cookie': ''
-            },
-            onComplete: function (response) {
-                console.log('/oauth2/token status', response.status);
+        xhr.open('POST', "https://api.twitter.com/oauth2/token");
+        
+        xhr.setRequestHeader('Authorization', 'Basic '+b64bearerToken);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
+        
+        xhr.responseType = "json";
+        
+        xhr.addEventListener('load', e => {
+            console.log('/oauth2/token status', xhr.status);
+            
+            if(xhr.status < 400){
+                var token = xhr.response.access_token;
 
-                var token = response.json.access_token;
-                
                 tokenCache.set(bearerTokenCredentails, token);
                 
                 accessTokenDef.resolve(token);
-                //console.log('accessToken', token);
-            },
-            content: 'grant_type=client_credentials'
-        }).post();
+            }
+            else{
+                accessTokenDef.reject('/oauth2/token error '+ xhr.status);
+            }
+            
+        });
+        
+        xhr.addEventListener('error', e => {
+            accessTokenDef.reject('/oauth2/token error '+ String(e));
+        })
+        
+        xhr.send('grant_type=client_credentials');
     }
     
     return accessTokenDef.promise;
