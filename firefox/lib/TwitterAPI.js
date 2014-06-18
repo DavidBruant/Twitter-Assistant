026765c0a3c7876650ccb3415ab4ae1a502037a5
David Bruant <bruant.d@gmail.com>
2014-06-19 01:12:14 +0200

    tweetsMine refactoring + histogram

--- firefox/lib/TwitterAPI.js
+++ firefox/lib/TwitterAPI.js
@@ -10,13 +10,19 @@ module.exports = function TwitterAPI(accessToken){
         getUserTimeline : function getUserTimeline(twittername){
             var deferred = defer();
 
+            var reqStart = Date.now();
+            
             Request({
                 url: 'https://api.twitter.com/1.1/statuses/user_timeline.json?count=200&screen_name='+twittername,
                 headers: {
                     'Authorization': 'Bearer '+accessToken
                 },
                 onComplete: function (response) {
-                    console.log('/1.1/statuses/user_timeline.json status', response.status);
+                    console.log(
+                        '/1.1/statuses/user_timeline.json status',
+                        response.status, 
+                        ((Date.now() - reqStart)/1000).toFixed(1)+'s'
+                    );
 
                     deferred.resolve(response.json);
                 }
