c1c664a727732a1d48e15df567dca841ef0de872
Matthieu Bué <matthieu.bue@gmail.com>
2015-03-10 03:48:47 +0100

    UI v1.0
    Twitter's code is "not easy", so I had to duplicate part of the
    div.WhoToFollow from its code.
    twitter-stylesheets.css and main.min.js are just for demo.

---
+++ UI-demo/js/main.js
@@ -0,0 +1,29 @@
+$(function() {
+	for(i=0 ; i<150 ; i++){
+		$(".TA-activity").append(
+			$("<div/>")
+				.addClass("TA-bar")
+				.attr("data-value",Math.round(Math.random() * 100))
+				.append($("<div/>")
+					.addClass("TA-tweets")
+					.css("height",Math.random() * 20 + 10 + "%")
+				)
+				.append($("<div/>")
+					.addClass("TA-links")
+					.css("height",Math.random() * 20 + "%")
+				)
+				.append($("<div/>")
+					.addClass("TA-medias")
+					.css("height",Math.random() * 10 + "%")
+				)
+				.append($("<div/>")
+					.addClass("TA-replies")
+					.css("height",Math.random() * 20 + "%")
+				)
+				.append($("<div/>")
+					.addClass("TA-retweets")
+					.css("height",Math.random() * 20 + "%")
+				)
+		);
+	}
+});
