6e4f2615bf20c5adbd0f234f5e7fdf0b14620927
David Bruant <bruant.d@gmail.com>
2015-03-15 11:37:32 +0100

    estimate visualized

--- firefox/data/metrics-integration/components/TweetsPerDayEstimation.ts
+++ firefox/data/metrics-integration/components/TweetsPerDayEstimation.ts
@@ -10,13 +10,13 @@ const TweetsPerDayEstimation = React.createClass({
     render: function(){ 
         const props : TweetsPerDayEstimationProps = this.props;
         
-        console.log('estimate', props.estimate)
+        console.log('estimate', props.estimate);
 
-        return React.DOM.section({className: 'tweets-per-day-estimation'}, []
-            /*React.DOM.p({}, [
-                React.DOM.strong({}, "Reminder: "),
-                "A human being is more than the few metrics you see above."
-            ])*/
+        return React.DOM.section(
+            {className: 'tweets-per-day-estimation'},
+            props.addonUserAlreadyFollowingVisitedUser ?
+                'Following this account contributes ~'+ props.estimate.toFixed(2) + ' tweets per day to your timeline' :
+                'Following this account would add ~'+ props.estimate.toFixed(2) + ' tweets per day to your timeline'
         );
     }
 });
