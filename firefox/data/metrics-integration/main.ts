aa749d5e141ceaab7820b6a37f77aa4eae38187f
Sphinx / Julien <julien.gattelier@gmail.com>
2015-10-31 10:50:17 +0100

    Fixed typo in relative path for ES6 d.ts file

--- firefox/data/metrics-integration/main.ts
+++ firefox/data/metrics-integration/main.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../../node_modules/typescript/bin/lib.es6.d.ts" />
+/// <reference path="../../../node_modules/typescript/lib/lib.es6.d.ts" />
 /// <reference path="../../defs/ES6.d.ts" />
 /// <reference path="../../defs/react-0.11.d.ts" />
 /// <reference path="../../defs/TwitterAPI.d.ts" />
